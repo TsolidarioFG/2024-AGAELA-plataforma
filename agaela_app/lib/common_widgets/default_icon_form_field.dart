39f6ff7856e19b537482e54f65b1466da5059998
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-04-20 21:44:39 +0200

    Added default icon form field

---
+++ agaela_app/lib/common_widgets/default_icon_form_field.dart
@@ -0,0 +1,37 @@
+import 'package:agaela_app/common_widgets/default_text_field.dart';
+import 'package:flutter/material.dart';
+
+class DefaultIconFormField extends StatelessWidget {
+  const DefaultIconFormField(
+      {super.key,
+      required this.controller,
+      required this.icon,
+      required this.text,
+      required this.sensitiveInformation});
+
+  final TextEditingController controller;
+
+  final Icon icon;
+
+  final String text;
+
+  final bool sensitiveInformation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+      child: Row(
+        children: <Widget>[
+          icon,
+          Expanded(
+            child: DefaultTextField(
+                controller: controller,
+                text: text,
+                sensitiveInformation: sensitiveInformation),
+          )
+        ],
+      ),
+    );
+  }
+}
