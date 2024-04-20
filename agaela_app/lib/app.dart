2b04032d937313d374e76dc29bd363b9991844fd
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-04-20 20:48:05 +0200

    Added localization config to app and title text

--- agaela_app/lib/app.dart
+++ agaela_app/lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:agaela_app/routing/router.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,6 +9,9 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: router,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
     );
   }
 }
