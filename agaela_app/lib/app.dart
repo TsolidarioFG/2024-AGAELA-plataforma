bbfeb53d5514869884247eeb20a22756139e55a5
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-04-20 20:32:34 +0200

    Added router to app

--- agaela_app/lib/app.dart
+++ agaela_app/lib/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:agaela_app/routing/router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp.router(
+      routerConfig: router,
+    );
   }
 }
