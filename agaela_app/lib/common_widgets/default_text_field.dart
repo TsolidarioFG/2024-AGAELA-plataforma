43e0acffd5aa31aa991d2e675a8dc5aa1e2c7a77
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-04-20 21:43:50 +0200

    Added default text field

---
+++ agaela_app/lib/common_widgets/default_text_field.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class DefaultTextField extends StatelessWidget {
+  const DefaultTextField(
+      {super.key,
+      required this.controller,
+      required this.text,
+      required this.sensitiveInformation});
+
+  final TextEditingController controller;
+
+  final String text;
+
+  final bool sensitiveInformation;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: sensitiveInformation,
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        labelText: text,
+      ),
+      style: const TextStyle(
+        decoration: TextDecoration.none,
+      ),
+      validator: (String? value) {
+        return (value == null || value.isEmpty)
+            ? AppLocalizations.of(context)!.errorEmptyField
+            : null;
+      },
+    );
+  }
+}
