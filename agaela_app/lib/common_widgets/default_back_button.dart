5f18d6c3d95f9fc95b0068e01d9af4ef5b5319f1
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-04-21 12:06:09 +0200

    Added default back button

---
+++ agaela_app/lib/common_widgets/default_back_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class DefaultBackButton extends StatelessWidget {
+  const DefaultBackButton({super.key, required this.backPage});
+
+  final VoidCallback? backPage;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.grey),
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            overlayColor: MaterialStateProperty.all<Color>(Colors.blueGrey),
+            minimumSize: MaterialStateProperty.all<Size>(const Size(200, 100)),
+            maximumSize: MaterialStateProperty.all<Size>(const Size(300, 200))),
+        onPressed: backPage,
+        child: Text(AppLocalizations.of(context)!.commonBackButtonText));
+  }
+}
