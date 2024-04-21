dc47916d6577d167ca02969084cb5f280e7cffdb
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-04-21 11:42:02 +0200

    Added routing from login to password recovery

--- agaela_app/lib/features/login/screens/login.dart
+++ agaela_app/lib/features/login/screens/login.dart
@@ -1,8 +1,10 @@
 import 'package:agaela_app/common_widgets/agaela_image_appbar.dart';
 import 'package:agaela_app/common_widgets/default_button.dart';
 import 'package:agaela_app/common_widgets/default_icon_form_field.dart';
+import 'package:agaela_app/routing/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:go_router/go_router.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -41,7 +43,8 @@ class _LoginState extends State<Login> {
                 function: () => {},
                 text: AppLocalizations.of(context)!.loginButton),
             TextButton(
-                onPressed: () => {},
+                onPressed: () =>
+                    context.goNamed(RoutesNames.passwordRecovery.name),
                 child:
                     Text(AppLocalizations.of(context)!.loginPasswordRecovery))
           ],
--- agaela_app/lib/routing/router.dart
+++ agaela_app/lib/routing/router.dart
@@ -1,11 +1,16 @@
 import 'package:agaela_app/features/login/screens/login.dart';
+import 'package:agaela_app/features/password_recovery/screens/password_recovery.dart';
 import 'package:go_router/go_router.dart';
 
-enum RoutesNames { login }
+enum RoutesNames { login, passwordRecovery }
 
 final GoRouter router = GoRouter(initialLocation: '/login', routes: [
   GoRoute(
       name: RoutesNames.login.name,
       path: '/login',
-      builder: (context, state) => const Login())
+      builder: (context, state) => const Login()),
+  GoRoute(
+      name: RoutesNames.passwordRecovery.name,
+      path: '/password_recovery',
+      builder: (context, state) => const PasswordRecovery())
 ]);
