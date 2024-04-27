9cf3ed3710f887f2f70194ee1567b1bdc0208e06
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-04-27 12:17:55 +0200

    Changed restore password mock implementation

--- agaela_app/lib/features/password_recovery/services/password_recovery_service_mock.dart
+++ agaela_app/lib/features/password_recovery/services/password_recovery_service_mock.dart
@@ -1,10 +1,10 @@
-import 'dart:io';
-
 import 'package:agaela_app/features/password_recovery/services/password_recovery_service.dart';
 
 class PasswordRecoveryServiceMock implements PasswordRecoveryService {
   @override
   Future<void> restorePassword(String dni) async {
-    sleep(const Duration(seconds: 1));
+    const errorText = 'error';
+    await Future.delayed(const Duration(seconds: 1));
+    if (dni == errorText) throw Exception();
   }
 }
