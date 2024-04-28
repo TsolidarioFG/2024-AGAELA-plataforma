0d0d5af31331944d5ca4ecbe77d264f4b08219b4
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-04-28 17:55:48 +0200

    Added request logic to edit password

--- agaela_app/lib/features/edit_password/screens/edit_password.dart
+++ agaela_app/lib/features/edit_password/screens/edit_password.dart
@@ -1,9 +1,15 @@
+import 'package:agaela_app/common_widgets/default_alert_dialog.dart';
 import 'package:agaela_app/common_widgets/default_icon_form_field.dart';
 import 'package:agaela_app/common_widgets/default_send_buttons.dart';
 import 'package:agaela_app/common_widgets/text_appbar.dart';
+import 'package:agaela_app/features/edit_password/services/edit_password_service.dart';
+import 'package:agaela_app/features/login/models/logged_user_provider.dart';
+import 'package:agaela_app/locators.dart';
+import 'package:agaela_app/routing/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 class EditPassword extends StatefulWidget {
   const EditPassword({super.key});
@@ -13,10 +19,59 @@ class EditPassword extends StatefulWidget {
 }
 
 class _EditPasswordState extends State<EditPassword> {
+  final EditPasswordService _editPasswordService =
+      locator<EditPasswordService>();
+
   final _editPasswordFormKey = GlobalKey<FormState>();
   final _oldPasswordController = TextEditingController();
   final _newPasswordController = TextEditingController();
   final _repeatNewPasswordController = TextEditingController();
+
+  bool _startedRequest = false;
+
+  bool _arePasswordsTheSame() {
+    if (_newPasswordController.text != _repeatNewPasswordController.text) {
+      showDefaultAlertDialog(
+          context,
+          const Icon(Icons.no_encryption),
+          AppLocalizations.of(context)!.editPasswordErrorPasswordsNotMatch,
+          () => GoRouter.of(context).pop());
+      return false;
+    }
+    return true;
+  }
+
+  void _finishEditPassword() {
+    setState(() {
+      _startedRequest = false;
+    });
+  }
+
+  void _startEditPassword() {
+    if (_arePasswordsTheSame()) {
+      setState(() {
+        _startedRequest = true;
+        Future<void> request = _editPasswordService.editPassword(
+            _oldPasswordController.text, _newPasswordController.text);
+        request.whenComplete(() => _finishEditPassword());
+        request.then(
+            (_) => showDefaultAlertDialog(
+                context,
+                const Icon(Icons.done),
+                AppLocalizations.of(context)!.editPasswordSuccessfulDescription,
+                () => Provider.of<LoggedUserProvider>(context, listen: false)
+                        .loggedUser!
+                        .isCarer
+                    ? () => {}
+                    : context.goNamed(RoutesNames.home.name)),
+            onError: (_) => showDefaultAlertDialog(
+                context,
+                const Icon(Icons.priority_high),
+                AppLocalizations.of(context)!.editPasswordErrorDescription,
+                () => GoRouter.of(context).pop()));
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,9 +103,11 @@ class _EditPasswordState extends State<EditPassword> {
                   text: AppLocalizations.of(context)!
                       .editPasswordRepeatNewPassword,
                   sensitiveInformation: true),
-              DefaultSendButtons(
-                  sendFunction: () => {},
-                  backPage: () => GoRouter.of(context).pop())
+              _startedRequest
+                  ? const CircularProgressIndicator()
+                  : DefaultSendButtons(
+                      sendFunction: () => _startEditPassword(),
+                      backPage: () => GoRouter.of(context).pop())
             ],
           ),
         ));
