56a89ecc3ccc05db152a04dc31fed87cfd3b0191
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-04-28 11:13:21 +0200

    Added request to login screen and provider to manage state

--- agaela_app/lib/app.dart
+++ agaela_app/lib/app.dart
@@ -1,20 +1,25 @@
-import 'package:flutter/material.dart';
+import 'package:agaela_app/features/login/models/logged_user_provider.dart';
 import 'package:agaela_app/routing/router.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => LoggedUserProvider(),
+      child: MaterialApp.router(
+        routerConfig: router,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+          useMaterial3: true,
+        ),
       ),
     );
   }
--- agaela_app/lib/features/login/screens/login.dart
+++ agaela_app/lib/features/login/screens/login.dart
@@ -1,10 +1,16 @@
 import 'package:agaela_app/common_widgets/agaela_image_appbar.dart';
+import 'package:agaela_app/common_widgets/default_alert_dialog.dart';
 import 'package:agaela_app/common_widgets/default_button.dart';
 import 'package:agaela_app/common_widgets/default_icon_form_field.dart';
+import 'package:agaela_app/features/login/models/logged_user.dart';
+import 'package:agaela_app/features/login/models/logged_user_provider.dart';
+import 'package:agaela_app/features/login/services/login_service.dart';
+import 'package:agaela_app/locators.dart';
 import 'package:agaela_app/routing/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -14,10 +20,38 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final LoginService _loginService = locator<LoginService>();
   final _loginFormKey = GlobalKey<FormState>();
 
   final _dniController = TextEditingController();
   final _passwordController = TextEditingController();
+
+  bool _startedRequest = false;
+
+  void _finishLogin() {
+    setState(() {
+      _startedRequest = false;
+    });
+  }
+
+  void _startLogin() {
+    setState(() {
+      _startedRequest = true;
+      Future<LoggedUser> request =
+          _loginService.login(_dniController.text, _passwordController.text);
+      request.whenComplete(() => _finishLogin());
+      request.then(
+          (loggedUser) => {
+                Provider.of<LoggedUserProvider>(context, listen: false)
+                    .setLoggedUser(loggedUser)
+              },
+          onError: (_) => showDefaultAlertDialog(
+              context,
+              const Icon(Icons.no_encryption),
+              AppLocalizations.of(context)!.loginErrorDescription,
+              () => GoRouter.of(context).pop()));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,9 +73,11 @@ class _LoginState extends State<Login> {
                 icon: const Icon(Icons.lock),
                 text: AppLocalizations.of(context)!.loginPasswordField,
                 sensitiveInformation: true),
-            DefaultButton(
-                function: () => {},
-                text: AppLocalizations.of(context)!.loginButton),
+            _startedRequest
+                ? const CircularProgressIndicator()
+                : DefaultButton(
+                    function: () => _startLogin(),
+                    text: AppLocalizations.of(context)!.loginButton),
             TextButton(
                 onPressed: () =>
                     context.goNamed(RoutesNames.passwordRecovery.name),
