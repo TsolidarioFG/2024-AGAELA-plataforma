7a16da04a2a294e4c82c7538342adee03588ebe4
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-04 21:43:51 +0200

    Added default send-cancel buttons

---
+++ agaela_app/lib/common_widgets/default_send_cancel_buttons.dart
@@ -0,0 +1,27 @@
+import 'package:agaela_app/common_widgets/default_button.dart';
+import 'package:agaela_app/common_widgets/default_cancel_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class DefaultSendCancelButtons extends StatelessWidget {
+  const DefaultSendCancelButtons(
+      {super.key, required this.sendFunction, required this.cancelFunction});
+
+  final VoidCallback sendFunction;
+
+  final VoidCallback cancelFunction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: <Widget>[
+      Expanded(
+          child: DefaultButton(
+              function: sendFunction,
+              text: AppLocalizations.of(context)!.commonSendButtonText)),
+      Expanded(
+          child: DefaultCancelButton(
+        cancelFunction: cancelFunction,
+      ))
+    ]);
+  }
+}
