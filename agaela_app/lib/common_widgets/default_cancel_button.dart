ead184ce2130091301f28f1ec799a4e0af6008f2
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-04 21:43:24 +0200

    Added cancel button

---
+++ agaela_app/lib/common_widgets/default_cancel_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class DefaultCancelButton extends StatelessWidget {
+  const DefaultCancelButton({super.key, required this.cancelFunction});
+
+  final VoidCallback cancelFunction;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            overlayColor: MaterialStateProperty.all<Color>(Colors.redAccent),
+            minimumSize: MaterialStateProperty.all<Size>(const Size(200, 100)),
+            maximumSize: MaterialStateProperty.all<Size>(const Size(300, 200))),
+        onPressed: cancelFunction,
+        child: Text(AppLocalizations.of(context)!.commonErrorButtonText));
+  }
+}
