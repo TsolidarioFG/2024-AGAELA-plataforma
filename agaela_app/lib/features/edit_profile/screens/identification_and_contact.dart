91e2beb217fb0fb0ce654a3aa1b41c1fc262a755
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-04 21:31:21 +0200

    Added new elements to identification and contact screen

--- agaela_app/lib/features/edit_profile/screens/identification_and_contact.dart
+++ agaela_app/lib/features/edit_profile/screens/identification_and_contact.dart
@@ -1,6 +1,10 @@
+import 'package:agaela_app/common_widgets/default_named_form_field.dart';
 import 'package:agaela_app/common_widgets/text_appbar.dart';
+import 'package:agaela_app/features/edit_profile/models/user_profile_information.dart';
+import 'package:agaela_app/features/edit_profile/models/user_profile_information_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 
 class IdentificationAndContact extends StatefulWidget {
   const IdentificationAndContact({super.key});
@@ -13,6 +17,23 @@ class IdentificationAndContact extends StatefulWidget {
 class _IdentificationAndContactState extends State<IdentificationAndContact> {
   final _identificationAndContactFormKey = GlobalKey<FormState>();
 
+  final _nameController = TextEditingController();
+  final _lastName1Controller = TextEditingController();
+  final _lastName2Controller = TextEditingController();
+  final _dniController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    UserProfileInformation userInformation =
+        Provider.of<UserProfileInformationProvider>(context, listen: false)
+            .userProfileInformation!;
+    _nameController.text = userInformation.name;
+    _lastName1Controller.text = userInformation.lastName1;
+    _lastName2Controller.text = userInformation.lastName2;
+    _dniController.text = userInformation.dni;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,13 +42,35 @@ class _IdentificationAndContactState extends State<IdentificationAndContact> {
             .editProfileIdentificationAndContactTitle,
       ),
       body: Form(
-        key: _identificationAndContactFormKey,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[],
-        ),
-      ),
+          key: _identificationAndContactFormKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: ListView(
+            children: <Widget>[
+              DefaultNamedFormField(
+                  controller: _nameController,
+                  name: AppLocalizations.of(context)!
+                      .editProfileIdentificationAndContactNameField,
+                  sensitiveInformation: false),
+              const Divider(),
+              DefaultNamedFormField(
+                  controller: _lastName1Controller,
+                  name: AppLocalizations.of(context)!
+                      .editProfileIdentificationAndContactLastName1Field,
+                  sensitiveInformation: false),
+              const Divider(),
+              DefaultNamedFormField(
+                  controller: _lastName2Controller,
+                  name: AppLocalizations.of(context)!
+                      .editProfileIdentificationAndContactLastName2Field,
+                  sensitiveInformation: false),
+              const Divider(),
+              DefaultNamedFormField(
+                  controller: _dniController,
+                  name: AppLocalizations.of(context)!
+                      .editProfileIdentificationAndContactDniField,
+                  sensitiveInformation: false)
+            ],
+          )),
     );
   }
 }
