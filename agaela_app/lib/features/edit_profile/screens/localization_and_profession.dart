372fca093c6925ef0ea31f3da1612e8fd9a4166f
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-06 12:28:14 +0200

    Added text fields to localization and profession screen

--- agaela_app/lib/features/edit_profile/screens/localization_and_profession.dart
+++ agaela_app/lib/features/edit_profile/screens/localization_and_profession.dart
@@ -1,6 +1,10 @@
+import 'package:agaela_app/common_widgets/default_named_form_field.dart';
 import 'package:agaela_app/common_widgets/text_appbar.dart';
+import 'package:agaela_app/features/edit_profile/models/user_profile_information.dart';
+import 'package:agaela_app/features/edit_profile/models/user_profile_information_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 
 class LocalizationAndProfession extends StatefulWidget {
   const LocalizationAndProfession({super.key});
@@ -11,12 +15,60 @@ class LocalizationAndProfession extends StatefulWidget {
 }
 
 class _LocalizationAndProfessionState extends State<LocalizationAndProfession> {
+  final _localizationAndProfessionFormKey = GlobalKey<FormState>();
+
+  final _cityController = TextEditingController();
+  final _postalCodeController = TextEditingController();
+  final _addressController = TextEditingController();
+  final _professionController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    UserProfileInformation userInformation =
+        Provider.of<UserProfileInformationProvider>(context, listen: false)
+            .userProfileInformation!;
+    _cityController.text = userInformation.city;
+    _postalCodeController.text = userInformation.postalCode;
+    _addressController.text = userInformation.address;
+    _professionController.text = userInformation.profession;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TextAppbar(
           text: AppLocalizations.of(context)!
               .editProfileLocalizationAndProfessionTitle),
+      body: Form(
+        key: _localizationAndProfessionFormKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            DefaultNamedFormField(
+                controller: _cityController,
+                name: AppLocalizations.of(context)!
+                    .editProfileLocalizationAndProfessionCityField,
+                sensitiveInformation: false),
+            DefaultNamedFormField(
+                controller: _postalCodeController,
+                name: AppLocalizations.of(context)!
+                    .editProfileLocalizationAndProfessionPostalCodeField,
+                sensitiveInformation: false),
+            DefaultNamedFormField(
+                controller: _addressController,
+                name: AppLocalizations.of(context)!
+                    .editProfileLocalizationAndProfessionAddressField,
+                sensitiveInformation: false),
+            DefaultNamedFormField(
+                controller: _professionController,
+                name: AppLocalizations.of(context)!
+                    .editProfileLocalizationAndProfessionProfessionField,
+                sensitiveInformation: false)
+          ],
+        ),
+      ),
     );
   }
 }
