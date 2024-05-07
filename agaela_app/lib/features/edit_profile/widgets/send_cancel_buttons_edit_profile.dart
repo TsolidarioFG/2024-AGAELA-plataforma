370e186bc978b21fdba09879895bbf77b6cee79d
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-07 19:44:32 +0200

    Added send cancel buttons edit profile widget

---
+++ agaela_app/lib/features/edit_profile/widgets/send_cancel_buttons_edit_profile.dart
@@ -0,0 +1,71 @@
+import 'package:agaela_app/common_widgets/default_alert_dialog.dart';
+import 'package:agaela_app/common_widgets/default_send_cancel_buttons.dart';
+import 'package:agaela_app/features/edit_profile/models/user_profile_information.dart';
+import 'package:agaela_app/features/edit_profile/models/user_profile_information_provider.dart';
+import 'package:agaela_app/features/edit_profile/services/edit_profile_service.dart';
+import 'package:agaela_app/locators.dart';
+import 'package:agaela_app/routing/router.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+typedef CreateUserFunction = UserProfileInformation Function();
+
+class SendCancelButtonsEditProfile extends StatefulWidget {
+  final CreateUserFunction createUser;
+
+  const SendCancelButtonsEditProfile({super.key, required this.createUser});
+
+  @override
+  State<SendCancelButtonsEditProfile> createState() =>
+      _SendCancelButtonsEditProfileState();
+}
+
+class _SendCancelButtonsEditProfileState
+    extends State<SendCancelButtonsEditProfile> {
+  final EditProfileService _editProfileService = locator<EditProfileService>();
+  Future<void>? _saveChangesRequest;
+
+  void _saveChanges() {
+    UserProfileInformation userInformation =
+        Provider.of<UserProfileInformationProvider>(context, listen: false)
+            .userProfileInformation!;
+    UserProfileInformation user = widget.createUser();
+    setState(() {
+      _saveChangesRequest = _editProfileService.setUserProfileInformation(
+          userInformation.dni, user);
+      _saveChangesRequest!.then(
+          (_) => {
+                Provider.of<UserProfileInformationProvider>(context,
+                        listen: false)
+                    .setUserProfileInformation(user),
+                showDefaultAlertDialog(
+                    context,
+                    const Icon(Icons.check),
+                    AppLocalizations.of(context)!.editProfileSavedChanges,
+                    () => context.goNamed(RoutesNames.editProfile.name))
+              },
+          onError: (_) => showDefaultAlertDialog(
+              context,
+              const Icon(Icons.error),
+              AppLocalizations.of(context)!.editProfileErrorSavingChanges,
+              () => context.goNamed(RoutesNames.editProfile.name)));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _saveChangesRequest,
+      builder: (BuildContext context, AsyncSnapshot snapshot) =>
+          snapshot.connectionState == ConnectionState.waiting
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : DefaultSendCancelButtons(
+                  sendFunction: () => _saveChanges(),
+                  cancelFunction: () => GoRouter.of(context).pop()),
+    );
+  }
+}
