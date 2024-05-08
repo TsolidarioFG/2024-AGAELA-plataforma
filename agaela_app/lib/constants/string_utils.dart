1e7b44b0ce2f79be9e94a4344216cb98e885b358
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-08 13:02:47 +0200

    Added string utils to constants

---
+++ agaela_app/lib/constants/string_utils.dart
@@ -0,0 +1,28 @@
+extension StringUtils on String {
+  bool get isValidEmail {
+    final emailRegExp = RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
+    return emailRegExp.hasMatch(this);
+  }
+
+  bool get isValidDni {
+    final emailRegExp = RegExp(r'^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$');
+    return emailRegExp.hasMatch(this);
+  }
+
+  bool get isValidName {
+    final nameRegExp =
+        RegExp(r"^\s*([A-Za-z]{1,}([\.,] |[-']| ))+[A-Za-z]+\.?\s*$");
+    return nameRegExp.hasMatch(this);
+  }
+
+  bool get isValidPassword {
+    final passwordRegExp = RegExp(
+        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\><*~]).{8,}/pre>');
+    return passwordRegExp.hasMatch(this);
+  }
+
+  bool get isValidPhone {
+    final phoneRegExp = RegExp(r'^\+?0[0-9]{10}$');
+    return phoneRegExp.hasMatch(this);
+  }
+}
