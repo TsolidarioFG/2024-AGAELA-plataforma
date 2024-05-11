c170cd4027a9721fd65e7a5375415813338cc333
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-11 17:35:25 +0200

    Added basic carer home screen

---
+++ agaela_app/lib/features/forms/screens/carer_home.dart
@@ -0,0 +1,13 @@
+import 'package:agaela_app/common_widgets/agaela_image_appbar.dart';
+import 'package:flutter/material.dart';
+
+class CarerHome extends StatelessWidget {
+  const CarerHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AgaelaImageAppbar(),
+    );
+  }
+}
