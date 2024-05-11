8dd3dd5e21d937dac22fc94d9fb93968c049a308
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-11 17:52:37 +0200

    Added buttons and icons to carer home screen

--- agaela_app/lib/features/forms/screens/carer_home.dart
+++ agaela_app/lib/features/forms/screens/carer_home.dart
@@ -1,5 +1,9 @@
 import 'package:agaela_app/common_widgets/agaela_image_appbar.dart';
+import 'package:agaela_app/common_widgets/default_button.dart';
+import 'package:agaela_app/routing/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:go_router/go_router.dart';
 
 class CarerHome extends StatelessWidget {
   const CarerHome({super.key});
@@ -8,6 +12,55 @@ class CarerHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AgaelaImageAppbar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              const Icon(Icons.edit),
+              Expanded(
+                child: DefaultButton(
+                  function: () => {},
+                  text: AppLocalizations.of(context)!.carerHomeCaredForms,
+                ),
+              ),
+              const Icon(Icons.supervisor_account),
+              Expanded(
+                child: DefaultButton(
+                    function: () => {},
+                    text: AppLocalizations.of(context)!
+                        .carerHomeCaregiverOverload),
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              const Icon(Icons.notification_important),
+              Expanded(
+                  child: DefaultButton(
+                      function: () => {},
+                      text: AppLocalizations.of(context)!
+                          .carerHomeNotifications)),
+              const Icon(Icons.account_circle),
+              Expanded(
+                  child: DefaultButton(
+                      function: () =>
+                          context.goNamed(RoutesNames.editProfile.name),
+                      text: AppLocalizations.of(context)!.carerHomeProfile))
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              const Icon(Icons.receipt),
+              Expanded(
+                  child: DefaultButton(
+                      function: () => {},
+                      text: AppLocalizations.of(context)!
+                          .carerHomeReportIncident)),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
