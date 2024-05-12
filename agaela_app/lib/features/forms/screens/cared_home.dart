2b17df8aefdff079e1d943b5243353f469deab72
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-12 11:22:03 +0200

    Added all the buttons to cared home screen

--- agaela_app/lib/features/forms/screens/cared_home.dart
+++ agaela_app/lib/features/forms/screens/cared_home.dart
@@ -1,14 +1,57 @@
+import 'package:agaela_app/common_widgets/default_back_button.dart';
+import 'package:agaela_app/common_widgets/default_button.dart';
 import 'package:agaela_app/features/forms/widgets/careds_dropdown.dart';
+import 'package:agaela_app/routing/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:go_router/go_router.dart';
 
 class CaredHome extends StatelessWidget {
   const CaredHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
-        children: <Widget>[CaredsDropdown()],
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          const Center(
+            child: CaredsDropdown(),
+          ),
+          Row(
+            children: <Widget>[
+              const Icon(Icons.feed),
+              Expanded(
+                  child: DefaultButton(
+                      function: () => {},
+                      text: AppLocalizations.of(context)!
+                          .caredHomeFunctionalState)),
+              const Icon(Icons.contact_page),
+              Expanded(
+                  child: DefaultButton(
+                      function: () => {},
+                      text: AppLocalizations.of(context)!
+                          .caredHomeSocialProcedures))
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              const Icon(Icons.account_circle),
+              Expanded(
+                  child: DefaultButton(
+                      function: () =>
+                          context.goNamed(RoutesNames.editProfile.name),
+                      text: AppLocalizations.of(context)!.caredHomeProfile)),
+              const Icon(Icons.receipt),
+              Expanded(
+                  child: DefaultButton(
+                      function: () => {},
+                      text: AppLocalizations.of(context)!
+                          .caredHomeReportIncident)),
+            ],
+          ),
+          DefaultBackButton(backPage: () => GoRouter.of(context).pop())
+        ],
       ),
     );
   }
