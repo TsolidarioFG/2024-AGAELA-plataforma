90c1710fcd81268ca5366e624748076598ab89db
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-12 11:00:33 +0200

    Added careds dropdown widget

---
+++ agaela_app/lib/features/forms/widgets/careds_dropdown.dart
@@ -0,0 +1,52 @@
+import 'package:agaela_app/common_widgets/text_bold_style.dart';
+import 'package:agaela_app/features/login/models/cared.dart';
+import 'package:agaela_app/features/login/models/carer.dart';
+import 'package:agaela_app/features/login/models/logged_user_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CaredsDropdown extends StatefulWidget {
+  const CaredsDropdown({super.key});
+
+  @override
+  State<CaredsDropdown> createState() => _CaredsDropdownState();
+}
+
+class _CaredsDropdownState extends State<CaredsDropdown> {
+  late List<Cared> _careds;
+
+  late Carer _carer;
+
+  late Cared _dropdownValue;
+
+  @override
+  void initState() {
+    super.initState();
+    _carer = Provider.of<LoggedUserProvider>(context, listen: false).loggedUser
+        as Carer;
+    _careds = _carer.careds;
+    _dropdownValue = _careds.first;
+  }
+
+  void _changeDropdownValue(Cared? value) {
+    setState(() {
+      _dropdownValue = value!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _careds.length == 1
+        ? TextBoldStyle(text: '${_careds.first.name} ${_careds.first.lastName}')
+        : DropdownMenu<Cared>(
+            initialSelection: _dropdownValue,
+            dropdownMenuEntries:
+                _careds.map<DropdownMenuEntry<Cared>>((Cared value) {
+              return DropdownMenuEntry(
+                  value: value, label: '${value.name} ${value.lastName}');
+            }).toList(),
+            onSelected: (Cared? value) {
+              _changeDropdownValue(value);
+            });
+  }
+}
