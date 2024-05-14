f5cf45e5e8ad026e9337ccbf78976f5d4572a608
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-14 10:31:55 +0200

    Adapted click color button to be a form field

--- agaela_app/lib/features/edit_profile/widgets/click_color_button.dart
+++ agaela_app/lib/features/edit_profile/widgets/click_color_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ClickColorButton extends StatefulWidget {
+class ClickColorButton extends FormField<bool> {
   final VoidCallback function;
 
   final bool initialState;
@@ -9,53 +9,30 @@ class ClickColorButton extends StatefulWidget {
 
   final String notClickedText;
 
-  const ClickColorButton(
+  ClickColorButton(
       {super.key,
       required this.function,
       required this.initialState,
       required this.clickedText,
-      required this.notClickedText});
-  @override
-  State<ClickColorButton> createState() => _ClickColorButtonState();
-}
-
-class _ClickColorButtonState extends State<ClickColorButton> {
-  late bool _clicked;
-
-  late String _text;
-
-  void _changeTextByClicked() {
-    _clicked ? _text = widget.clickedText : _text = widget.notClickedText;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _clicked = widget.initialState;
-    _changeTextByClicked();
-  }
-
-  void _changeClicked() {
-    setState(() {
-      _clicked = !_clicked;
-      _changeTextByClicked();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-        style: ButtonStyle(
-            backgroundColor: _clicked
-                ? MaterialStateProperty.all<Color>(Colors.deepPurple)
-                : MaterialStateProperty.all<Color>(Colors.grey),
-            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-            overlayColor: _clicked
-                ? MaterialStateProperty.all<Color>(Colors.purple)
-                : MaterialStateProperty.all<Color>(Colors.blueGrey),
-            minimumSize: MaterialStateProperty.all<Size>(const Size(200, 100)),
-            maximumSize: MaterialStateProperty.all<Size>(const Size(300, 200))),
-        onPressed: () => {widget.function(), _changeClicked()},
-        child: Text(_text));
-  }
+      required this.notClickedText})
+      : super(
+            initialValue: initialState,
+            builder: (FormFieldState<bool> state) {
+              return ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: state.value!
+                          ? MaterialStateProperty.all<Color>(Colors.deepPurple)
+                          : MaterialStateProperty.all<Color>(Colors.grey),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      overlayColor: state.value!
+                          ? MaterialStateProperty.all<Color>(Colors.purple)
+                          : MaterialStateProperty.all<Color>(Colors.blueGrey),
+                      minimumSize:
+                          MaterialStateProperty.all<Size>(const Size(200, 100)),
+                      maximumSize: MaterialStateProperty.all<Size>(
+                          const Size(300, 200))),
+                  onPressed: () => {function(), state.didChange(!state.value!)},
+                  child: Text(state.value! ? clickedText : notClickedText));
+            });
 }
