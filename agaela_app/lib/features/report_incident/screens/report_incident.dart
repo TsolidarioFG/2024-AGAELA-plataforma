91f0d4d777bc168557ae84ed6c7af36c32538b09
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-14 20:07:06 +0200

    Added basic implementation to report incident screen

--- agaela_app/lib/features/report_incident/screens/report_incident.dart
+++ agaela_app/lib/features/report_incident/screens/report_incident.dart
@@ -1,4 +1,6 @@
+import 'package:agaela_app/common_widgets/default_send_buttons.dart';
 import 'package:agaela_app/common_widgets/text_appbar.dart';
+import 'package:agaela_app/utils/go_home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -10,12 +12,34 @@ class ReportIncident extends StatefulWidget {
 }
 
 class _ReportIncidentState extends State<ReportIncident> {
+  final _reportIncidentFormKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: TextAppbar(
-        text: AppLocalizations.of(context)!.reportIncidentTitle,
-      ),
-    );
+        appBar: TextAppbar(
+          text: AppLocalizations.of(context)!.reportIncidentTitle,
+        ),
+        body: Form(
+          key: _reportIncidentFormKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              TextFormField(
+                  minLines: 5,
+                  maxLines: 5,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), errorMaxLines: 3),
+                  validator: (String? incident) {
+                    return incident == null || incident.isEmpty
+                        ? AppLocalizations.of(context)!.errorEmptyField
+                        : null;
+                  }),
+              DefaultSendButtons(
+                  sendFunction: () => {}, backPage: () => goToHome(context))
+            ],
+          ),
+        ));
   }
 }
