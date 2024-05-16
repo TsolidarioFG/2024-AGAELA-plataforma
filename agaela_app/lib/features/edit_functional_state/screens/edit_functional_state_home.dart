d8a2d89b2f86589f3c4cdf1c4c2f4ba3b71acfd3
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-16 18:56:46 +0200

    Added the rest of elements to edit functional state home

--- agaela_app/lib/features/edit_functional_state/screens/edit_functional_state_home.dart
+++ agaela_app/lib/features/edit_functional_state/screens/edit_functional_state_home.dart
@@ -1,4 +1,7 @@
+import 'package:agaela_app/common_widgets/default_back_button.dart';
+import 'package:agaela_app/common_widgets/default_button.dart';
 import 'package:agaela_app/common_widgets/text_appbar.dart';
+import 'package:agaela_app/utils/go_home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -10,6 +13,31 @@ class EditFunctionalStateHome extends StatelessWidget {
     return Scaffold(
       appBar: TextAppbar(
         text: AppLocalizations.of(context)!.editFunctionalStateHomeTitle,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              const Icon(Icons.library_add_check),
+              Expanded(
+                child: DefaultButton(
+                  function: () => {},
+                  text: AppLocalizations.of(context)!
+                      .editFunctionalStateRatingScale,
+                ),
+              ),
+              const Icon(Icons.edit_document),
+              Expanded(
+                  child: DefaultButton(
+                function: () => {},
+                text: AppLocalizations.of(context)!
+                    .editFunctionalStateBarthelIndex,
+              ))
+            ],
+          ),
+          DefaultBackButton(backPage: () => goToHome(context))
+        ],
       ),
     );
   }
