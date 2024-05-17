1c1ed09169f6b88a25fae34db7893c2a49621cc7
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-17 12:21:39 +0200

    Added get previous answers request to edit functional state form

--- agaela_app/lib/features/edit_functional_state/screens/edit_functional_state_form.dart
+++ agaela_app/lib/features/edit_functional_state/screens/edit_functional_state_form.dart
@@ -2,8 +2,12 @@ import 'package:agaela_app/common_widgets/text_appbar.dart';
 import 'package:agaela_app/common_widgets/text_bold_style.dart';
 import 'package:agaela_app/features/edit_functional_state/models/actual_form.dart';
 import 'package:agaela_app/features/edit_functional_state/models/actual_form_provider.dart';
+import 'package:agaela_app/features/edit_functional_state/services/edit_functional_state_service.dart';
 import 'package:agaela_app/features/edit_functional_state/widgets/edit_functional_state_form_footer.dart';
 import 'package:agaela_app/features/forms/models/question.dart';
+import 'package:agaela_app/features/login/models/logged_user.dart';
+import 'package:agaela_app/features/login/models/logged_user_provider.dart';
+import 'package:agaela_app/locators.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -16,78 +20,107 @@ class EditFunctionalStateForm extends StatefulWidget {
 }
 
 class _EditFunctionalStateFormState extends State<EditFunctionalStateForm> {
+  final EditFunctionalStateService _editFunctionalStateService =
+      locator<EditFunctionalStateService>();
+
   final inexistentId = -1;
   late String _title;
   late List<Question> _questions;
 
   Map<int, int>? _answersSelecteds;
 
+  Future<Map<int, int>?>? _previousAnswers;
+
   @override
   void initState() {
     ActualForm actualForm =
         Provider.of<ActualFormProvider>(context, listen: false).actualForm!;
+    LoggedUser actualUser =
+        Provider.of<LoggedUserProvider>(context, listen: false).loggedUser!;
     super.initState();
     _title = actualForm.title;
     _questions = actualForm.questions;
     _answersSelecteds = {};
-    for (Question question in _questions) {
-      _answersSelecteds![question.id] = inexistentId;
-    }
+    _previousAnswers = _editFunctionalStateService.getPreviousAnswers(
+        actualForm.formId, actualUser.selectedId);
+    _previousAnswers!.then((answers) {
+      if (answers == null) {
+        for (Question question in _questions) {
+          _answersSelecteds![question.id] = inexistentId;
+        }
+      } else {
+        _answersSelecteds!.addAll(answers);
+      }
+    }, onError: (_) => {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TextAppbar(text: _title),
-      body: ListView.builder(
-          padding: const EdgeInsets.all(10),
-          itemCount: _questions.length,
-          itemBuilder: (BuildContext context, int questionIndex) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                TextBoldStyle(
-                  text: _questions[questionIndex].title,
-                  textMaxLines: 3,
-                ),
-                ListView.builder(
-                    shrinkWrap: true,
-                    physics: const ClampingScrollPhysics(),
-                    itemCount: _questions[questionIndex].answers.length,
-                    itemBuilder: (BuildContext context, int answerIndex) {
-                      int questionId = _questions[questionIndex].id;
-                      int answerId =
-                          _questions[questionIndex].answers[answerIndex].id;
-                      String answerText =
-                          _questions[questionIndex].answers[answerIndex].text;
-                      return ListTile(
-                        title: ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                _answersSelecteds![questionId] = answerId;
-                              });
-                            },
-                            style: ButtonStyle(
-                              backgroundColor:
-                                  _answersSelecteds![questionId] == answerId
-                                      ? MaterialStateProperty.all<Color>(
-                                          Colors.deepPurple)
-                                      : MaterialStateProperty.all<Color>(
-                                          Colors.grey),
-                              foregroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.white),
-                              overlayColor: MaterialStateProperty.all<Color>(
-                                  Colors.purple),
-                            ),
-                            child: Text(
-                              answerText,
-                              maxLines: 5,
-                            )),
-                      );
-                    })
-              ],
-            );
-          }),
+      body: FutureBuilder(
+        future: _previousAnswers,
+        builder: (BuildContext context, AsyncSnapshot snapshot) => snapshot
+                    .connectionState ==
+                ConnectionState.waiting
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                padding: const EdgeInsets.all(10),
+                itemCount: _questions.length,
+                itemBuilder: (BuildContext context, int questionIndex) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      TextBoldStyle(
+                        text: _questions[questionIndex].title,
+                        textMaxLines: 3,
+                      ),
+                      ListView.builder(
+                          shrinkWrap: true,
+                          physics: const ClampingScrollPhysics(),
+                          itemCount: _questions[questionIndex].answers.length,
+                          itemBuilder: (BuildContext context, int answerIndex) {
+                            int questionId = _questions[questionIndex].id;
+                            int answerId = _questions[questionIndex]
+                                .answers[answerIndex]
+                                .id;
+                            String answerText = _questions[questionIndex]
+                                .answers[answerIndex]
+                                .text;
+                            return ListTile(
+                              title: ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _answersSelecteds![questionId] = answerId;
+                                    });
+                                  },
+                                  style: ButtonStyle(
+                                    backgroundColor:
+                                        _answersSelecteds![questionId] ==
+                                                answerId
+                                            ? MaterialStateProperty.all<Color>(
+                                                Colors.deepPurple)
+                                            : MaterialStateProperty.all<Color>(
+                                                Colors.grey),
+                                    foregroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                            Colors.white),
+                                    overlayColor:
+                                        MaterialStateProperty.all<Color>(
+                                            Colors.purple),
+                                  ),
+                                  child: Text(
+                                    answerText,
+                                    maxLines: 5,
+                                  )),
+                            );
+                          })
+                    ],
+                  );
+                }),
+      ),
       bottomNavigationBar: EditFunctionalStateFormFooter(
           sendFunction:
               _answersSelecteds!.containsValue(inexistentId) ? null : () => {},
