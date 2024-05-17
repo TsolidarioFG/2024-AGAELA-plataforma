743f3e90fc46400e4a7f1e582520b338b731771c
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-17 17:49:03 +0200

    Adapted form request to be generic and changed error localization

---
+++ agaela_app/lib/common_widgets/form_request_widget.dart
@@ -0,0 +1,71 @@
+import 'package:agaela_app/common_widgets/default_alert_dialog.dart';
+import 'package:agaela_app/features/edit_functional_state/models/actual_form.dart';
+import 'package:agaela_app/features/edit_functional_state/models/actual_form_provider.dart';
+import 'package:agaela_app/features/edit_functional_state/services/edit_functional_state_service.dart';
+import 'package:agaela_app/features/forms/models/question.dart';
+import 'package:agaela_app/locators.dart';
+import 'package:agaela_app/routing/router.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+class FormRequestWidget extends StatefulWidget {
+  const FormRequestWidget(
+      {super.key,
+      required this.child,
+      required this.title,
+      required this.formId,
+      required this.started});
+
+  final Widget child;
+
+  final ValueNotifier<String> title;
+
+  final ValueNotifier<int> formId;
+
+  final ValueNotifier<bool> started;
+
+  @override
+  State<FormRequestWidget> createState() => _FormRequestWidgetState();
+}
+
+class _FormRequestWidgetState extends State<FormRequestWidget> {
+  final EditFunctionalStateService _editFunctionalStateService =
+      locator<EditFunctionalStateService>();
+
+  Future<List<Question>>? _request;
+
+  void _startForm(String title, int formId) {
+    setState(() {
+      _request = _editFunctionalStateService.getFormQuestions(formId);
+      _request!.then((questions) {
+        ActualForm actualForm = ActualForm(formId, title, questions);
+        Provider.of<ActualFormProvider>(context, listen: false)
+            .setActualForm(actualForm);
+        context.goNamed(RoutesNames.editFunctionalStateForm.name);
+      },
+          onError: (_) => showDefaultAlertDialog(
+              context,
+              const Icon(Icons.error),
+              AppLocalizations.of(context)!.errorGettingForm,
+              () => GoRouter.of(context).pop()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.started.value) {
+      _startForm(widget.title.value, widget.formId.value);
+    }
+    return FutureBuilder(
+        future: _request,
+        builder:
+            (BuildContext context, AsyncSnapshot<List<Question>> snapshot) =>
+                snapshot.connectionState == ConnectionState.waiting
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : widget.child);
+  }
+}
--- agaela_app/lib/features/edit_functional_state/screens/edit_functional_state_home.dart
+++ agaela_app/lib/features/edit_functional_state/screens/edit_functional_state_home.dart
@@ -1,18 +1,10 @@
-import 'package:agaela_app/common_widgets/default_alert_dialog.dart';
 import 'package:agaela_app/common_widgets/default_back_button.dart';
 import 'package:agaela_app/common_widgets/default_button.dart';
+import 'package:agaela_app/common_widgets/form_request_widget.dart';
 import 'package:agaela_app/common_widgets/text_appbar.dart';
-import 'package:agaela_app/features/edit_functional_state/models/actual_form.dart';
-import 'package:agaela_app/features/edit_functional_state/models/actual_form_provider.dart';
-import 'package:agaela_app/features/edit_functional_state/services/edit_functional_state_service.dart';
-import 'package:agaela_app/features/forms/models/question.dart';
-import 'package:agaela_app/locators.dart';
-import 'package:agaela_app/routing/router.dart';
 import 'package:agaela_app/utils/go_home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 
 class EditFunctionalStateHome extends StatefulWidget {
   const EditFunctionalStateHome({super.key});
@@ -23,26 +15,15 @@ class EditFunctionalStateHome extends StatefulWidget {
 }
 
 class _EditFunctionalStateHomeState extends State<EditFunctionalStateHome> {
-  final EditFunctionalStateService _editFunctionalStateService =
-      locator<EditFunctionalStateService>();
+  final ValueNotifier<int> _formId = ValueNotifier<int>(-1);
+  final ValueNotifier<String> _title = ValueNotifier<String>('');
+  final ValueNotifier<bool> _startRequest = ValueNotifier<bool>(false);
 
-  Future<List<Question>>? _request;
-
-  void _startForm(String title, int formId) {
+  void _startFormRequest(String title, int formId) {
     setState(() {
-      _request = _editFunctionalStateService.getFormQuestions(formId);
-      _request!.then((questions) {
-        ActualForm actualForm = ActualForm(formId, title, questions);
-        Provider.of<ActualFormProvider>(context, listen: false)
-            .setActualForm(actualForm);
-        context.goNamed(RoutesNames.editFunctionalStateForm.name);
-      },
-          onError: (_) => showDefaultAlertDialog(
-              context,
-              const Icon(Icons.error),
-              AppLocalizations.of(context)!
-                  .editFunctionalStateHomeErrorDescription,
-              () => GoRouter.of(context).pop()));
+      _title.value = title;
+      _formId.value = formId;
+      _startRequest.value = true;
     });
   }
 
@@ -52,45 +33,41 @@ class _EditFunctionalStateHomeState extends State<EditFunctionalStateHome> {
         appBar: TextAppbar(
           text: AppLocalizations.of(context)!.editFunctionalStateHomeTitle,
         ),
-        body: FutureBuilder(
-          future: _request,
-          builder:
-              (BuildContext context, AsyncSnapshot<List<Question>> snapshot) =>
-                  snapshot.connectionState == ConnectionState.waiting
-                      ? const Center(
-                          child: CircularProgressIndicator(),
-                        )
-                      : Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Row(
-                              children: <Widget>[
-                                const Icon(Icons.library_add_check),
-                                Expanded(
-                                  child: DefaultButton(
-                                    function: () => _startForm(
-                                        AppLocalizations.of(context)!
-                                            .editFunctionalStateRatingScaleTitle,
-                                        1),
-                                    text: AppLocalizations.of(context)!
-                                        .editFunctionalStateRatingScale,
-                                  ),
-                                ),
-                                const Icon(Icons.edit_document),
-                                Expanded(
-                                    child: DefaultButton(
-                                  function: () => _startForm(
-                                      AppLocalizations.of(context)!
-                                          .editFunctionalStateBarthelIndexTitle,
-                                      2),
-                                  text: AppLocalizations.of(context)!
-                                      .editFunctionalStateBarthelIndex,
-                                ))
-                              ],
-                            ),
-                            DefaultBackButton(backPage: () => goToHome(context))
-                          ],
-                        ),
+        body: FormRequestWidget(
+          title: _title,
+          formId: _formId,
+          started: _startRequest,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  const Icon(Icons.library_add_check),
+                  Expanded(
+                    child: DefaultButton(
+                      function: () => _startFormRequest(
+                          AppLocalizations.of(context)!
+                              .editFunctionalStateRatingScaleTitle,
+                          1),
+                      text: AppLocalizations.of(context)!
+                          .editFunctionalStateRatingScale,
+                    ),
+                  ),
+                  const Icon(Icons.edit_document),
+                  Expanded(
+                      child: DefaultButton(
+                    function: () => _startFormRequest(
+                        AppLocalizations.of(context)!
+                            .editFunctionalStateBarthelIndexTitle,
+                        2),
+                    text: AppLocalizations.of(context)!
+                        .editFunctionalStateBarthelIndex,
+                  ))
+                ],
+              ),
+              DefaultBackButton(backPage: () => goToHome(context))
+            ],
+          ),
         ));
   }
 }
