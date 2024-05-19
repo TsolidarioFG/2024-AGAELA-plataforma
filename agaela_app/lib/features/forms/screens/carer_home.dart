05f4650f6aa5b99da4f1a646433d266492196cf6
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-19 13:25:08 +0200

    Adapted carer home screen to be scrolleable

--- agaela_app/lib/features/forms/screens/carer_home.dart
+++ agaela_app/lib/features/forms/screens/carer_home.dart
@@ -1,6 +1,7 @@
 import 'package:agaela_app/common_widgets/agaela_image_appbar.dart';
 import 'package:agaela_app/common_widgets/default_button.dart';
 import 'package:agaela_app/common_widgets/form_request_widget.dart';
+import 'package:agaela_app/common_widgets/scrolleable_widget.dart';
 import 'package:agaela_app/constants/global_constants.dart';
 import 'package:agaela_app/routing/router.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +16,8 @@ class CarerHome extends StatefulWidget {
 }
 
 class _CarerHomeState extends State<CarerHome> {
+  final double height = 20.0;
+
   final ValueNotifier<int> _formId = ValueNotifier<int>(-1);
   final ValueNotifier<String> _title = ValueNotifier<String>('');
   final ValueNotifier<bool> _startRequest = ValueNotifier<bool>(false);
@@ -32,64 +35,76 @@ class _CarerHomeState extends State<CarerHome> {
     return Scaffold(
         appBar: AgaelaImageAppbar(),
         body: FormRequestWidget(
-          title: _title,
-          formId: _formId,
-          started: _startRequest,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Row(
+            title: _title,
+            formId: _formId,
+            started: _startRequest,
+            child: ScrolleableWidget(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  const Icon(Icons.edit),
-                  Expanded(
-                    child: DefaultButton(
-                      function: () =>
-                          context.goNamed(RoutesNames.caredHome.name),
-                      text: AppLocalizations.of(context)!.carerHomeCaredForms,
-                    ),
+                  SizedBox(
+                    height: height,
                   ),
-                  const Icon(Icons.supervisor_account),
-                  Expanded(
-                    child: DefaultButton(
-                        function: () => _startFormRequest(
-                            caregiverOverloadFormId,
-                            AppLocalizations.of(context)!
-                                .carerHomeCaregiverOverloadTitle),
-                        text: AppLocalizations.of(context)!
-                            .carerHomeCaregiverOverload),
-                  )
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  const Icon(Icons.notification_important),
-                  Expanded(
-                      child: DefaultButton(
-                          function: () => {},
-                          text: AppLocalizations.of(context)!
-                              .carerHomeNotifications)),
-                  const Icon(Icons.account_circle),
-                  Expanded(
-                      child: DefaultButton(
+                  Row(
+                    children: <Widget>[
+                      const Icon(Icons.edit),
+                      Expanded(
+                        child: DefaultButton(
                           function: () =>
-                              context.goNamed(RoutesNames.editProfile.name),
-                          text: AppLocalizations.of(context)!.carerHomeProfile))
+                              context.goNamed(RoutesNames.caredHome.name),
+                          text:
+                              AppLocalizations.of(context)!.carerHomeCaredForms,
+                        ),
+                      ),
+                      const Icon(Icons.supervisor_account),
+                      Expanded(
+                        child: DefaultButton(
+                            function: () => _startFormRequest(
+                                caregiverOverloadFormId,
+                                AppLocalizations.of(context)!
+                                    .carerHomeCaregiverOverloadTitle),
+                            text: AppLocalizations.of(context)!
+                                .carerHomeCaregiverOverload),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: height,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      const Icon(Icons.notification_important),
+                      Expanded(
+                          child: DefaultButton(
+                              function: () => {},
+                              text: AppLocalizations.of(context)!
+                                  .carerHomeNotifications)),
+                      const Icon(Icons.account_circle),
+                      Expanded(
+                          child: DefaultButton(
+                              function: () =>
+                                  context.goNamed(RoutesNames.editProfile.name),
+                              text: AppLocalizations.of(context)!
+                                  .carerHomeProfile))
+                    ],
+                  ),
+                  SizedBox(
+                    height: height,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      const Icon(Icons.receipt),
+                      Expanded(
+                          child: DefaultButton(
+                              function: () => context
+                                  .goNamed(RoutesNames.reportIncident.name),
+                              text: AppLocalizations.of(context)!
+                                  .carerHomeReportIncident)),
+                      const Expanded(child: SizedBox(child: null))
+                    ],
+                  ),
                 ],
               ),
-              Row(
-                children: <Widget>[
-                  const Icon(Icons.receipt),
-                  Expanded(
-                      child: DefaultButton(
-                          function: () =>
-                              context.goNamed(RoutesNames.reportIncident.name),
-                          text: AppLocalizations.of(context)!
-                              .carerHomeReportIncident)),
-                  const Expanded(child: SizedBox(child: null))
-                ],
-              ),
-            ],
-          ),
-        ));
+            )));
   }
 }
