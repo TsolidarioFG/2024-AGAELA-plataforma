d3a59c1dadaf2e56125ac71e29fceff1bcb3d609
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-05-19 13:25:44 +0200

    Adapted default home screen to be scrolleable

--- agaela_app/lib/features/forms/screens/default_home.dart
+++ agaela_app/lib/features/forms/screens/default_home.dart
@@ -1,5 +1,6 @@
 import 'package:agaela_app/common_widgets/agaela_image_appbar.dart';
 import 'package:agaela_app/common_widgets/default_button.dart';
+import 'package:agaela_app/common_widgets/scrolleable_widget.dart';
 import 'package:agaela_app/routing/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -8,56 +9,72 @@ import 'package:go_router/go_router.dart';
 class DefaultHome extends StatelessWidget {
   const DefaultHome({super.key});
 
+  final double height = 20.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AgaelaImageAppbar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Row(
+        appBar: AgaelaImageAppbar(),
+        body: ScrolleableWidget(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              const Icon(Icons.feed),
-              Expanded(
-                  child: DefaultButton(
-                      function: () =>
-                          context.goNamed(RoutesNames.editFunctionalState.name),
-                      text: AppLocalizations.of(context)!.homeFunctionalState)),
-              const Icon(Icons.contact_page),
-              Expanded(
-                  child: DefaultButton(
-                      function: () => {},
-                      text: AppLocalizations.of(context)!.homeSocialProcedures))
+              SizedBox(
+                height: height,
+              ),
+              Row(
+                children: <Widget>[
+                  const Icon(Icons.feed),
+                  Expanded(
+                      child: DefaultButton(
+                          function: () => context
+                              .goNamed(RoutesNames.editFunctionalState.name),
+                          text: AppLocalizations.of(context)!
+                              .homeFunctionalState)),
+                  const Icon(Icons.contact_page),
+                  Expanded(
+                      child: DefaultButton(
+                          function: () => {},
+                          text: AppLocalizations.of(context)!
+                              .homeSocialProcedures))
+                ],
+              ),
+              SizedBox(
+                height: height,
+              ),
+              Row(
+                children: <Widget>[
+                  const Icon(Icons.notification_important),
+                  Expanded(
+                      child: DefaultButton(
+                          function: () => {},
+                          text:
+                              AppLocalizations.of(context)!.homeNotifications)),
+                  const Icon(Icons.account_circle),
+                  Expanded(
+                      child: DefaultButton(
+                          function: () =>
+                              context.goNamed(RoutesNames.editProfile.name),
+                          text: AppLocalizations.of(context)!.homeProfile))
+                ],
+              ),
+              SizedBox(
+                height: height,
+              ),
+              Row(
+                children: <Widget>[
+                  const Icon(Icons.receipt),
+                  Expanded(
+                      child: DefaultButton(
+                          function: () =>
+                              context.goNamed(RoutesNames.reportIncident.name),
+                          text: AppLocalizations.of(context)!
+                              .homeReportIncident)),
+                  const Expanded(child: SizedBox(child: null))
+                ],
+              ),
             ],
           ),
-          Row(
-            children: <Widget>[
-              const Icon(Icons.notification_important),
-              Expanded(
-                  child: DefaultButton(
-                      function: () => {},
-                      text: AppLocalizations.of(context)!.homeNotifications)),
-              const Icon(Icons.account_circle),
-              Expanded(
-                  child: DefaultButton(
-                      function: () =>
-                          context.goNamed(RoutesNames.editProfile.name),
-                      text: AppLocalizations.of(context)!.homeProfile))
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              const Icon(Icons.receipt),
-              Expanded(
-                  child: DefaultButton(
-                      function: () =>
-                          context.goNamed(RoutesNames.reportIncident.name),
-                      text: AppLocalizations.of(context)!.homeReportIncident)),
-              const Expanded(child: SizedBox(child: null))
-            ],
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
