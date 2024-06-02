ee31a24bfea836d335f2a869c46d867327af9923
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-06-02 12:13:29 +0200

    Added text instead of form id and navigated to specific form

--- agaela_app/lib/features/notifications/screens/notifications_home.dart
+++ agaela_app/lib/features/notifications/screens/notifications_home.dart
@@ -1,6 +1,8 @@
 import 'package:agaela_app/common_widgets/default_back_button.dart';
 import 'package:agaela_app/common_widgets/default_button.dart';
+import 'package:agaela_app/common_widgets/form_request_widget.dart';
 import 'package:agaela_app/common_widgets/text_appbar.dart';
+import 'package:agaela_app/constants/global_constants.dart';
 import 'package:agaela_app/features/login/models/carer.dart';
 import 'package:agaela_app/features/login/models/logged_user.dart';
 import 'package:agaela_app/features/login/models/logged_user_provider.dart';
@@ -21,6 +23,8 @@ class NotificationsHome extends StatefulWidget {
 class _NotificationsHomeState extends State<NotificationsHome> {
   late List<PendingForm> _pendingForms;
 
+  late final Map<int, String> _formsNames = <int, String>{};
+
   @override
   void initState() {
     super.initState();
@@ -29,10 +33,33 @@ class _NotificationsHomeState extends State<NotificationsHome> {
         .pendingForms;
   }
 
-  String getNotificationCaredText(int? caredId, int formId) {
+  final ValueNotifier<int> _formId = ValueNotifier<int>(-1);
+  final ValueNotifier<String> _title = ValueNotifier<String>('');
+  final ValueNotifier<bool> _startRequest = ValueNotifier<bool>(false);
+
+  void _startFormRequest(int formId, String title) {
+    setState(() {
+      _formId.value = formId;
+      _title.value = title;
+      _startRequest.value = true;
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    _formsNames[ratingScaleFormId] =
+        AppLocalizations.of(context)!.editFunctionalStateRatingScale;
+    _formsNames[barthelIndexFormId] =
+        AppLocalizations.of(context)!.editFunctionalStateBarthelIndex;
+    _formsNames[caregiverOverloadFormId] =
+        AppLocalizations.of(context)!.carerHomeCaregiverOverloadTitle;
+    super.didChangeDependencies();
+  }
+
+  String getNotificationCaredText(int? caredId, int formId, String formTitle) {
     if (caredId == null) {
       return AppLocalizations.of(context)!
-          .notificationsHomePendingNotification(formId);
+          .notificationsHomePendingNotification(formTitle);
     } else {
       LoggedUser actualUser =
           Provider.of<LoggedUserProvider>(context, listen: false).loggedUser!;
@@ -41,7 +68,7 @@ class _NotificationsHomeState extends State<NotificationsHome> {
           .firstWhere((cared) => caredId == cared.id)
           .name;
       return AppLocalizations.of(context)!
-          .notificationsHomePendingNotificationCared(formId, caredName);
+          .notificationsHomePendingNotificationCared(formTitle, caredName);
     }
   }
 
@@ -51,34 +78,42 @@ class _NotificationsHomeState extends State<NotificationsHome> {
         appBar: TextAppbar(
           text: AppLocalizations.of(context)!.notificationsHomeTitle,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            ListView.builder(
-                shrinkWrap: true,
-                physics: const ClampingScrollPhysics(),
-                itemCount: _pendingForms.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    leading: const Icon(Icons.notification_important),
-                    title: DefaultButton(
-                      function: () => {},
-                      text: Provider.of<LoggedUserProvider>(context,
-                                  listen: false)
-                              .loggedUser!
-                              .isCarer
-                          ? getNotificationCaredText(
-                              (_pendingForms[index] as PendingFormCarer)
-                                  .caredId,
-                              _pendingForms[index].formId)
-                          : AppLocalizations.of(context)!
-                              .notificationsHomePendingNotification(
-                                  _pendingForms[index].formId),
-                    ),
-                  );
-                }),
-            DefaultBackButton(backPage: () => goToHome(context))
-          ],
+        body: FormRequestWidget(
+          title: _title,
+          formId: _formId,
+          started: _startRequest,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ListView.builder(
+                  shrinkWrap: true,
+                  physics: const ClampingScrollPhysics(),
+                  itemCount: _pendingForms.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    int formId = _pendingForms[index].formId;
+                    String formTitle = _formsNames[formId]!;
+                    return ListTile(
+                      leading: const Icon(Icons.notification_important),
+                      title: DefaultButton(
+                        function: () => _startFormRequest(formId, formTitle),
+                        text: Provider.of<LoggedUserProvider>(context,
+                                    listen: false)
+                                .loggedUser!
+                                .isCarer
+                            ? getNotificationCaredText(
+                                (_pendingForms[index] as PendingFormCarer)
+                                    .caredId,
+                                formId,
+                                formTitle)
+                            : AppLocalizations.of(context)!
+                                .notificationsHomePendingNotification(
+                                    formTitle),
+                      ),
+                    );
+                  }),
+              DefaultBackButton(backPage: () => goToHome(context))
+            ],
+          ),
         ));
   }
 }
