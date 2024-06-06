9091a46210b49723323a4495402dc9d40ac3427e
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-06-06 13:03:07 +0200

    Added base url to global constants

--- agaela_app/lib/constants/global_constants.dart
+++ agaela_app/lib/constants/global_constants.dart
@@ -2,3 +2,4 @@ const logo = 'assets/images/logo.png';
 const ratingScaleFormId = 1;
 const barthelIndexFormId = 2;
 const caregiverOverloadFormId = 3;
+const baseUrl = 'https://preproduccion.duacode.org/agaela/agaela-gestion/api';
