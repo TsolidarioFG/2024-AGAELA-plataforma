21068d351c526d8433b747586f2ae3d2cc8e0f75
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-06-17 19:22:30 +0200

    Added request to save changes in cards and income

--- agaela_app/lib/features/edit_social_procedures/screens/cards_and_income.dart
+++ agaela_app/lib/features/edit_social_procedures/screens/cards_and_income.dart
@@ -4,6 +4,7 @@ import 'package:agaela_app/common_widgets/list_button.dart';
 import 'package:agaela_app/common_widgets/text_appbar.dart';
 import 'package:agaela_app/common_widgets/text_bold_style.dart';
 import 'package:agaela_app/features/edit_social_procedures/services/edit_social_procedures_service.dart';
+import 'package:agaela_app/features/login/models/logged_user.dart';
 import 'package:agaela_app/features/login/models/logged_user_provider.dart';
 import 'package:agaela_app/locators.dart';
 import 'package:agaela_app/routing/router.dart';
@@ -33,11 +34,35 @@ class _CardsAndIncomeState extends State<CardsAndIncome> {
 
   Future<List<Map<String, String>>>? _getCardsAndIncomeTypesRequest;
 
+  Future<void>? _saveCardsAndIncomeRequest;
+
   final List<String> _selectedAnswers = [];
 
   void _checkCorrectValues() {
     setState(() {
       _correctValues = !_selectedAnswers.contains(incorrectValue);
+    });
+  }
+
+  void _saveCardsAndIncome() {
+    LoggedUser actualUser =
+        Provider.of<LoggedUserProvider>(context, listen: false).loggedUser!;
+    setState(() {
+      _saveCardsAndIncomeRequest = _editSocialProceduresService
+          .setCardsAndIncome(actualUser.getActualCode(), _selectedAnswers);
+      _saveCardsAndIncomeRequest!.then(
+          (_) => showDefaultAlertDialog(
+              context,
+              const Icon(Icons.check),
+              AppLocalizations.of(context)!
+                  .editSocialProceduresCardsAndIncomeSavedChanges,
+              () => context.goNamed(RoutesNames.editSocialProcedures.name)),
+          onError: (_) => showDefaultAlertDialog(
+              context,
+              const Icon(Icons.error),
+              AppLocalizations.of(context)!
+                  .editSocialProceduresCardsAndIncomeErrorSavingChanges,
+              () => GoRouter.of(context).pop()));
     });
   }
 
@@ -77,67 +102,75 @@ class _CardsAndIncomeState extends State<CardsAndIncome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: TextAppbar(
-        text:
-            '${AppLocalizations.of(context)!.editSocialProceduresCardsAndIncomeTitle} ${Provider.of<LoggedUserProvider>(context, listen: false).loggedUser!.getCaredName()}',
-      ),
-      body: FutureBuilder(
-        future: _getCardsAndIncomeTypesRequest,
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          return snapshot.connectionState == ConnectionState.waiting
-              ? const Center(
-                  child: CircularProgressIndicator(),
-                )
-              : ListView.builder(
-                  itemCount: _cardsAndIncomeTypes.length,
-                  itemBuilder: (BuildContext context, int cardsAndIncomeIndex) {
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        TextBoldStyle(
-                            text: _cardsAndIncomeTitles[cardsAndIncomeIndex]),
-                        ListView.builder(
-                          shrinkWrap: true,
-                          physics: const ClampingScrollPhysics(),
-                          itemCount:
-                              _cardsAndIncomeTypes[cardsAndIncomeIndex].length,
-                          itemBuilder: (BuildContext context, int typeIndex) {
-                            String title =
-                                _cardsAndIncomeTypes[cardsAndIncomeIndex]
-                                    .values
-                                    .elementAt(typeIndex);
-                            String key =
-                                _cardsAndIncomeTypes[cardsAndIncomeIndex]
-                                    .keys
-                                    .elementAt(typeIndex);
-                            String selectedKey =
-                                _selectedAnswers[cardsAndIncomeIndex];
-                            return ListTile(
-                              title: ListButton(
-                                onPressed: () {
-                                  setState(() {
-                                    _selectedAnswers[cardsAndIncomeIndex] = key;
-                                    _checkCorrectValues();
-                                  });
-                                },
-                                text: title,
-                                selected: selectedKey == key,
-                              ),
-                            );
-                          },
-                        )
-                      ],
-                    );
-                  },
-                );
-        },
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: DefaultSendCancelButtons(
-          sendFunction: _correctValues ? () => {} : null,
-          cancelFunction: () => GoRouter.of(context).pop(),
+        appBar: TextAppbar(
+          text:
+              '${AppLocalizations.of(context)!.editSocialProceduresCardsAndIncomeTitle} ${Provider.of<LoggedUserProvider>(context, listen: false).loggedUser!.getCaredName()}',
         ),
-      ),
-    );
+        body: FutureBuilder(
+          future: _getCardsAndIncomeTypesRequest,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            return snapshot.connectionState == ConnectionState.waiting
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ListView.builder(
+                    itemCount: _cardsAndIncomeTypes.length,
+                    itemBuilder:
+                        (BuildContext context, int cardsAndIncomeIndex) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          TextBoldStyle(
+                              text: _cardsAndIncomeTitles[cardsAndIncomeIndex]),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            physics: const ClampingScrollPhysics(),
+                            itemCount: _cardsAndIncomeTypes[cardsAndIncomeIndex]
+                                .length,
+                            itemBuilder: (BuildContext context, int typeIndex) {
+                              String title =
+                                  _cardsAndIncomeTypes[cardsAndIncomeIndex]
+                                      .values
+                                      .elementAt(typeIndex);
+                              String key =
+                                  _cardsAndIncomeTypes[cardsAndIncomeIndex]
+                                      .keys
+                                      .elementAt(typeIndex);
+                              String selectedKey =
+                                  _selectedAnswers[cardsAndIncomeIndex];
+                              return ListTile(
+                                title: ListButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _selectedAnswers[cardsAndIncomeIndex] =
+                                          key;
+                                      _checkCorrectValues();
+                                    });
+                                  },
+                                  text: title,
+                                  selected: selectedKey == key,
+                                ),
+                              );
+                            },
+                          )
+                        ],
+                      );
+                    },
+                  );
+          },
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: FutureBuilder(
+              future: _saveCardsAndIncomeRequest,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                return snapshot.connectionState == ConnectionState.waiting
+                    ? const Center(child: CircularProgressIndicator())
+                    : DefaultSendCancelButtons(
+                        sendFunction:
+                            _correctValues ? () => _saveCardsAndIncome() : null,
+                        cancelFunction: () => GoRouter.of(context).pop(),
+                      );
+              }),
+        ));
   }
 }
