d4de066dc371abc9d92aae04048fbaa2f0b03248
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-06-17 10:32:57 +0200

    Added basic send cancel buttons and logic to cards and income screen

--- agaela_app/lib/features/edit_social_procedures/screens/cards_and_income.dart
+++ agaela_app/lib/features/edit_social_procedures/screens/cards_and_income.dart
@@ -1,7 +1,9 @@
+import 'package:agaela_app/common_widgets/default_send_cancel_buttons.dart';
 import 'package:agaela_app/common_widgets/text_appbar.dart';
 import 'package:agaela_app/features/login/models/logged_user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 class CardsAndIncome extends StatefulWidget {
@@ -12,12 +14,39 @@ class CardsAndIncome extends StatefulWidget {
 }
 
 class _CardsAndIncomeState extends State<CardsAndIncome> {
+  final _cardsAndIncomeFormKey = GlobalKey<FormState>();
+
+  bool _formChanged = false;
+
+  void _changeForm() {
+    setState(() {
+      _formChanged = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TextAppbar(
         text:
             '${AppLocalizations.of(context)!.editSocialProceduresCardsAndIncomeTitle} ${Provider.of<LoggedUserProvider>(context, listen: false).loggedUser!.getCaredName()}',
+      ),
+      body: Form(
+        key: _cardsAndIncomeFormKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        onChanged: () => _changeForm,
+        child: ListView(
+          children: const <Widget>[],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: DefaultSendCancelButtons(
+          sendFunction: _formChanged
+              ? () =>
+                  {if (_cardsAndIncomeFormKey.currentState!.validate()) null}
+              : null,
+          cancelFunction: () => GoRouter.of(context).pop(),
+        ),
       ),
     );
   }
