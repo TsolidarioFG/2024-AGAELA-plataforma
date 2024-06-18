1be14942fd143bb3c0c56dbf02f42bb1101e38e1
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-06-18 10:39:28 +0200

    Added basic permanent work disability screen

---
+++ agaela_app/lib/features/edit_social_procedures/screens/permanent_work_disability.dart
@@ -0,0 +1,25 @@
+import 'package:agaela_app/common_widgets/text_appbar.dart';
+import 'package:agaela_app/features/login/models/logged_user_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+
+class PermanentWorkDisability extends StatefulWidget {
+  const PermanentWorkDisability({super.key});
+
+  @override
+  State<PermanentWorkDisability> createState() =>
+      _PermanentWorkDisabilityState();
+}
+
+class _PermanentWorkDisabilityState extends State<PermanentWorkDisability> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TextAppbar(
+        text:
+            '${AppLocalizations.of(context)!.editSocialProceduresPermanentWorkDisabilityTitle} ${Provider.of<LoggedUserProvider>(context, listen: false).loggedUser!.getCaredName()}',
+      ),
+    );
+  }
+}
