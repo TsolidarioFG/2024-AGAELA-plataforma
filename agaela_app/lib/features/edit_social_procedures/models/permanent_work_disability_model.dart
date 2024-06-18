c84989206f79edd7827f195e28530b979e62d34b
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-06-18 11:45:06 +0200

    Added permanent work disability model

---
+++ agaela_app/lib/features/edit_social_procedures/models/permanent_work_disability_model.dart
@@ -0,0 +1,8 @@
+class PermanentWorkDisabilityModel {
+  Map<String, String> processedTypes;
+  Map<String, String> resolvedDisabilityTypes;
+  Map<String, String> unresolvedProceduresTypes;
+
+  PermanentWorkDisabilityModel(this.processedTypes,
+      this.resolvedDisabilityTypes, this.unresolvedProceduresTypes);
+}
