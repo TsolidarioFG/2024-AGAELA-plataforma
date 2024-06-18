b74200c2f2095b8eea7659e0372f0c62a13a1af5
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-06-18 10:43:51 +0200

    Added bottomNavigationBar to permanent work disability screen

--- agaela_app/lib/features/edit_social_procedures/screens/permanent_work_disability.dart
+++ agaela_app/lib/features/edit_social_procedures/screens/permanent_work_disability.dart
@@ -1,7 +1,9 @@
+import 'package:agaela_app/common_widgets/default_send_cancel_buttons.dart';
 import 'package:agaela_app/common_widgets/text_appbar.dart';
 import 'package:agaela_app/features/login/models/logged_user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 class PermanentWorkDisability extends StatefulWidget {
@@ -19,6 +21,12 @@ class _PermanentWorkDisabilityState extends State<PermanentWorkDisability> {
       appBar: TextAppbar(
         text:
             '${AppLocalizations.of(context)!.editSocialProceduresPermanentWorkDisabilityTitle} ${Provider.of<LoggedUserProvider>(context, listen: false).loggedUser!.getCaredName()}',
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: DefaultSendCancelButtons(
+          sendFunction: () => {},
+          cancelFunction: () => GoRouter.of(context).pop(),
+        ),
       ),
     );
   }
