04db17a4b8dc1b6ad4d5cebd5cb764ed77cbac7f
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-06-19 18:18:06 +0200

    Added send cancel buttons to disability screen

--- agaela_app/lib/features/edit_social_procedures/screens/disability.dart
+++ agaela_app/lib/features/edit_social_procedures/screens/disability.dart
@@ -1,7 +1,9 @@
+import 'package:agaela_app/common_widgets/default_send_cancel_buttons.dart';
 import 'package:agaela_app/common_widgets/text_appbar.dart';
 import 'package:agaela_app/features/login/models/logged_user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 class Disability extends StatefulWidget {
@@ -18,6 +20,12 @@ class _DisabilityState extends State<Disability> {
       appBar: TextAppbar(
         text:
             '${AppLocalizations.of(context)!.editSocialProceduresDisabilityTitle} ${Provider.of<LoggedUserProvider>(context, listen: false).loggedUser!.getCaredName()}',
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: DefaultSendCancelButtons(
+          sendFunction: () => {},
+          cancelFunction: () => GoRouter.of(context).pop(),
+        ),
       ),
     );
   }
