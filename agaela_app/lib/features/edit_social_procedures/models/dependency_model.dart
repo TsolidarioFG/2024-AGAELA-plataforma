08d5dc3cee6a5342ef8cabd93f0a0083bfd108d4
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-06-20 13:20:02 +0200

    Added toJson method

--- agaela_app/lib/features/edit_social_procedures/models/dependency_model.dart
+++ agaela_app/lib/features/edit_social_procedures/models/dependency_model.dart
@@ -12,4 +12,14 @@ class DependencyModel {
   String? serviceClarifications;
 
   DependencyModel();
+
+  Map<String, dynamic> toJson() => {
+        "idTipoTramitado": processedTypeSelected,
+        "notificadaViaUrgencia": notifiedUrgently ? 1 : 0,
+        "idTipoGradoDependecia": dependencyLevelSelected,
+        "recibiendoPIA": individualizedAttentionPlan ? 1 : 0,
+        "recibiendoServicio": gettingServices ? 1 : 0,
+        "aclaracionesRecibiendoServicio": serviceClarifications,
+        "idTipoTramiteNoResuelto": unresolvedProcedureSelected,
+      };
 }
