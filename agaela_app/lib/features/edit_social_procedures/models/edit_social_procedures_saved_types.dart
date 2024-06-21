bfd245f5b3e09cafd6312c491b64e7c4c369b7a9
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-06-21 19:49:49 +0200

    Added edit social procedures saved types model

---
+++ agaela_app/lib/features/edit_social_procedures/models/edit_social_procedures_saved_types.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+
+class EditSocialProceduresSavedTypes extends ChangeNotifier {
+  Map<String, String>? healthCardTypes;
+  Map<String, String>? netIncomeTypes;
+  Map<String, String>? parkingCardTypes;
+  Map<String, String>? processedTypes;
+  Map<String, String>? resolvedDisabilityTypes;
+  Map<String, String>? unresolvedProceduresTypes;
+  Map<String, String>? dependencyOrdersOfPayment;
+  Map<String, String>? dependencyServices;
+}
