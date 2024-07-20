10ffc6b8fb7a513c74819e1ac513171711da39ae
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-07-20 09:43:28 +0200

    Modified notifications service to add init notifications method

--- agaela_app/lib/features/notifications/services/notifications_service.dart
+++ agaela_app/lib/features/notifications/services/notifications_service.dart
@@ -1,5 +1,9 @@
 import 'package:agaela_app/features/login/models/pending_form.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 abstract class NotificationsService {
   Future<List<PendingForm>> getNotifications(bool carer);
+
+  Future<void> initNotifications(
+      Future<void> Function(RemoteMessage) onNotification);
 }
--- agaela_app/lib/features/notifications/services/notifications_service_impl.dart
+++ agaela_app/lib/features/notifications/services/notifications_service_impl.dart
@@ -1,16 +1,25 @@
 import 'dart:convert';
 
 import 'package:agaela_app/constants/global_constants.dart';
+import 'package:agaela_app/features/login/models/logged_user.dart';
+import 'package:agaela_app/features/login/models/logged_user_provider.dart';
 import 'package:agaela_app/features/login/models/pending_form.dart';
 import 'package:agaela_app/features/login/models/pending_form_carer.dart';
 import 'package:agaela_app/features/login/models/pending_form_person_with_als.dart';
 import 'package:agaela_app/features/notifications/services/notifications_service.dart';
+import 'package:agaela_app/routing/router.dart';
 import 'package:agaela_app/utils/common_headers.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 const _notificationsPath = '/perfil/notificaciones';
 
 class NotificationsServiceImpl implements NotificationsService {
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
   @override
   Future<List<PendingForm>> getNotifications(bool carer) async {
     final response = await http.get(Uri.parse('$baseUrl$_notificationsPath'),
@@ -31,5 +40,40 @@ class NotificationsServiceImpl implements NotificationsService {
     } else {
       return [];
     }
+  }
+
+  Future<void> onMessageWhileAppRunning() async {
+    LoggedUser loggedUser = Provider.of<LoggedUserProvider>(
+            globalNavigatorKey.currentContext!,
+            listen: false)
+        .loggedUser!;
+    List<PendingForm> pendingForms = await getNotifications(loggedUser.isCarer);
+    Provider.of<LoggedUserProvider>(globalNavigatorKey.currentContext!,
+            listen: false)
+        .loggedUser!
+        .pendingForms = pendingForms;
+    GoRouter.of(globalNavigatorKey.currentContext!)
+        .goNamed(RoutesNames.notificationsHome.name);
+  }
+
+  @override
+  Future<void> initNotifications(
+      Future<void> Function(RemoteMessage) onNotification) async {
+    await _firebaseMessaging.requestPermission(
+        alert: true,
+        announcement: true,
+        badge: true,
+        carPlay: false,
+        criticalAlert: false,
+        provisional: false,
+        sound: true);
+    final token = await _firebaseMessaging.getToken();
+    debugPrint('Token: $token');
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      onMessageWhileAppRunning();
+    });
+    FirebaseMessaging.onMessageOpenedApp
+        .listen((RemoteMessage message) async => onMessageWhileAppRunning());
+    FirebaseMessaging.onBackgroundMessage(onNotification);
   }
 }
