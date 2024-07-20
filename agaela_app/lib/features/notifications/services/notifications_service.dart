1bb5efe06e645503150b596b338becc9d47f24f2
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-07-20 12:27:27 +0200

    Modified notifications with local notifications plugin and changed notifications service

--- agaela_app/lib/features/notifications/services/notifications_service.dart
+++ agaela_app/lib/features/notifications/services/notifications_service.dart
@@ -1,9 +1,7 @@
 import 'package:agaela_app/features/login/models/pending_form.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 
 abstract class NotificationsService {
   Future<List<PendingForm>> getNotifications(bool carer);
 
-  Future<void> initNotifications(
-      Future<void> Function(RemoteMessage) onNotification);
+  Future<void> initNotifications();
 }
--- agaela_app/lib/features/notifications/services/notifications_service_impl.dart
+++ agaela_app/lib/features/notifications/services/notifications_service_impl.dart
@@ -6,19 +6,45 @@ import 'package:agaela_app/features/login/models/logged_user_provider.dart';
 import 'package:agaela_app/features/login/models/pending_form.dart';
 import 'package:agaela_app/features/login/models/pending_form_carer.dart';
 import 'package:agaela_app/features/login/models/pending_form_person_with_als.dart';
+import 'package:agaela_app/features/login/services/login_service.dart';
 import 'package:agaela_app/features/notifications/services/notifications_service.dart';
+import 'package:agaela_app/locators.dart';
 import 'package:agaela_app/routing/router.dart';
 import 'package:agaela_app/utils/common_headers.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 
 const _notificationsPath = '/perfil/notificaciones';
 
+@pragma('vm:entry-point')
+Future<void> onBackgroundMessage(RemoteMessage message) async {
+  initLocator();
+  final loginService = locator<LoginService>();
+  loginService.loginWithToken().then((loggedUser) {
+    Provider.of<LoggedUserProvider>(globalNavigatorKey.currentContext!,
+            listen: false)
+        .setLoggedUser(loggedUser);
+    GoRouter.of(globalNavigatorKey.currentContext!)
+        .goNamed(RoutesNames.notificationsHome.name);
+  });
+}
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', 'High Importance Channel',
+    description: 'This channel is used for important notifications',
+    importance: Importance.max,
+    playSound: true,
+    enableLights: true);
+
 class NotificationsServiceImpl implements NotificationsService {
   final _firebaseMessaging = FirebaseMessaging.instance;
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
   @override
   Future<List<PendingForm>> getNotifications(bool carer) async {
@@ -42,29 +68,51 @@ class NotificationsServiceImpl implements NotificationsService {
     }
   }
 
-  Future<void> onMessageWhileAppRunning() async {
-    LoggedUser loggedUser = Provider.of<LoggedUserProvider>(
+  Future<void> updateNotifications() async {
+    LoggedUser? loggedUser = Provider.of<LoggedUserProvider>(
             globalNavigatorKey.currentContext!,
             listen: false)
-        .loggedUser!;
-    List<PendingForm> pendingForms = await getNotifications(loggedUser.isCarer);
-    Provider.of<LoggedUserProvider>(globalNavigatorKey.currentContext!,
+        .loggedUser;
+    if (loggedUser != null) {
+      List<PendingForm> pendingForms =
+          await getNotifications(loggedUser.isCarer);
+      Provider.of<LoggedUserProvider>(globalNavigatorKey.currentContext!,
+              listen: false)
+          .loggedUser!
+          .pendingForms = pendingForms;
+    }
+  }
+
+  Future<void> onMessageWhileAppRunning() async {
+    await updateNotifications();
+    final snackBar = SnackBar(
+      content: Text(AppLocalizations.of(globalNavigatorKey.currentContext!)!
+          .newNotificationText),
+    );
+    ScaffoldMessenger.of(globalNavigatorKey.currentContext!)
+        .showSnackBar(snackBar);
+  }
+
+  Future<void> onMessageOpenedWhileAppRunning() async {
+    await updateNotifications();
+    LoggedUser? loggedUser = Provider.of<LoggedUserProvider>(
+            globalNavigatorKey.currentContext!,
             listen: false)
-        .loggedUser!
-        .pendingForms = pendingForms;
-    GoRouter.of(globalNavigatorKey.currentContext!)
-        .goNamed(RoutesNames.notificationsHome.name);
+        .loggedUser;
+    if (loggedUser != null) {
+      GoRouter.of(globalNavigatorKey.currentContext!)
+          .goNamed(RoutesNames.notificationsHome.name);
+    }
   }
 
   @override
-  Future<void> initNotifications(
-      Future<void> Function(RemoteMessage) onNotification) async {
+  Future<void> initNotifications() async {
     await _firebaseMessaging.requestPermission(
         alert: true,
         announcement: true,
         badge: true,
         carPlay: false,
-        criticalAlert: false,
+        criticalAlert: true,
         provisional: false,
         sound: true);
     final token = await _firebaseMessaging.getToken();
@@ -72,8 +120,17 @@ class NotificationsServiceImpl implements NotificationsService {
     FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
       onMessageWhileAppRunning();
     });
-    FirebaseMessaging.onMessageOpenedApp
-        .listen((RemoteMessage message) async => onMessageWhileAppRunning());
-    FirebaseMessaging.onBackgroundMessage(onNotification);
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
+      onMessageOpenedWhileAppRunning();
+    });
+    FirebaseMessaging.onBackgroundMessage(onBackgroundMessage);
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+            alert: true, badge: true, sound: true);
   }
 }
