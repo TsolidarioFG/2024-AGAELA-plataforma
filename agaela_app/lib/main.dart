ce6c2fb8df22112df7d87144fbf5db1477fd7e2e
Sergio Goyanes Legazpi <sergio.legazpi@udc.es>
2024-07-20 09:44:13 +0200

    Added Firebase initialization in main method

--- agaela_app/lib/main.dart
+++ agaela_app/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:agaela_app/app.dart';
+import 'package:agaela_app/firebase_options.dart';
 import 'package:agaela_app/locators.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   initLocator();
+
   runApp(const App());
 }
